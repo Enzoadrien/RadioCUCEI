8879d92a4152fb062158066f6babefa8832c65ec
Enzoadrien <martinezquirozadrien89@gmail.com>
2021-10-14 14:19:34 -0500

    Se agrego logo y boton 14/10/21

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, StyleSheet, TouchableOpacity, SafeAreaView, Dimensions,Image} from 'react-native';
 import Sound from 'react-native-sound';
+import Slider from '@react-native-community/slider';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { STATEMENT_TYPES } from '@babel/types';
 
 
+const {width, height} = Dimensions.get('window');
 //Categoria del sonido.
 Sound.setCategory('Playback');
+
 
 
 var audio = new Sound(
@@ -17,19 +22,15 @@ var audio = new Sound(
       return;
     }
     // if loaded successfully
-    console.log(
-      'Duracion en segundos: ' +
-        audio.getDuration() +
-        'number of channels: ' +
-        audio.getNumberOfChannels(),
-    );
+    
   },
 );
 
 const App = () => {
   const [playing, setPlaying] = useState();
+
   useEffect(() => {
-    audio.setVolume(1);
+    audio.setVolume(0);
     return () => {
       audio.release();
     };
@@ -51,29 +52,106 @@ const App = () => {
       });
     }
   };
-  
+
+
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.playBtn} onPress={playPause}>
-       
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={estilos.contenedor}>
+      <View style={estilos.maincontenedor}>
+        <View style={estilos.contenedorimagen}>
+          <Image 
+          style={estilos.logo}
+          source={require('./src/imgs/Logo.png')}
+          />
+
+        </View>
+        <View>
+          <Slider 
+          style ={estilos.volumen}
+          minimumValue={0}
+          maximunValue={100}
+          step ={0.1}
+          thumbTintColor="orange"
+          minimumTrackTintColor="orange"
+          maximumTrackTintColor="#FFD369"
+          onSlidingComplete={()=>{}} />
+        </View>
+
+        <View style={estilos.playcontenedor}>
+          
+          <TouchableOpacity onPress={playPause}>
+            <Ionicons name={   "ios-pause-circle"} size={80} color="orange" />
+          </TouchableOpacity>
+
+        </View>
+          
+
+      </View>
+      <View style={estilos.bajocontenedor}>
+      
+  
+      </View>
+      
+    </SafeAreaView>
+   
+      
+
+      
+    
   );
 };
-const styles = StyleSheet.create({
-  container: {
+
+
+const estilos = StyleSheet.create({
+  contenedor: {
     flex: 1,
-    alignItems: 'center',
+    backgroundColor: '#242424',
+    
+  },
+  maincontenedor:{
+    flex:1,
     justifyContent: 'center',
-    backgroundColor: '#2f3640',
+    alignItems: 'center',
+  },
+  bajocontenedor:{
+    borderTopColor:'#393E46',
+    borderTopWidth:2,
+    width: '100%',
+    alignItems: 'center',
+    paddingVertical:35,
+  },
+  contenedorimagen:{
+    width:250,
+    height: 250,
+    marginBottom:100,
+
+  },
+  logo:{
+    width: '100%',
+    height: '100%',
+
+  },
+  volumen:{
+    width: 300,
+    height: 40,
+    marginTop:-50,
+    flexDirection: 'row',
+
+  },
+  playcontenedor:{
+    flexDirection: 'row',
+    width:'60%',
+    justifyContent: 'space-between',
+    marginTop: 15,
+    justifyContent: 'center',
   },
   playBtn: {
     width: '50%',
     height:50,
-    backgroundColor:'#c23616',
+    backgroundColor:'#f06c24',
     justifyContent: 'center',
     alignItems: 'center',
   }
+
 
 });
 
